abd0bb9c92f52bd20a9cff1828119b4946da32b9
jakelwestall <jacob@jwestall.com>
2021-10-10 14:52:30 -0500

    Added to muttable variables example

--- muttable_ex/src/main.rs
+++ muttable_ex/src/main.rs
@@ -1,13 +1,40 @@
+// Constants are always imuttable, and cannot be used with mut
+// They must be typed, and contain a value
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-	// The following code will fail, as you cannot reassign an imuttable variable
+
+}
+
+fn bad_code() {
+	// The following code will fail, as you cannot reassign an imuttable variable (commented the offending line so it will compile)
 	let x = 5;
 	println!("The value of x is {}", x);
-	x = 6;
+	// x = 6;
 	println!("The value of x is {}", x);
-	
+}
+
+fn good_code() {
 	// The following code will compile just fine, as y is muttable
 	let mut y = 5;
-	println!("The value of x is {}", y);
+	println!("The value of y is {}", y);
 	y = 6;
-	println!("The value of x is {}", y);
+	println!("The value of y is {}", y);
+}
+
+fn shaddowing_variable() {
+	// Shaddowing is redefining a variable with an already defined name
+	let x = 5;
+	let x = x + 1;
+	let x = x * 2;
+	
+	// Shaddowing lets us change the variable type as well
+	let spaces = "     ";
+	let spaces = spaces.len();
+	
+	// Muttables can't do this, and will fail to compile
+	// let mut spaces = "     ";
+	// spaces = spaces.len();
+	
+	println!("The value of x is {}", x);
 }
